00984a4647e73b95daa31e9633f7b2fa557d0d09
Peter Jones <pjones@pmade.com>
2008-11-18 14:54:43 -0700

    New file to help when testing controllers

---
+++ lib/bolt/mocking.rb
@@ -0,0 +1,50 @@
+################################################################################
+#
+# Copyright (C) 2006-2008 pmade inc. (Peter Jones pjones@pmade.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+module Mocking
+
+  ################################################################################
+  module ControllerInstanceMethods
+    
+    ################################################################################
+    def logged_in?
+      !@mock_login.nil?
+    end
+    
+    ################################################################################
+    def current_user
+      @mock_login || Bolt::Config.user_model_class.new
+    end
+
+    ################################################################################
+    def current_user= (user)
+      @mock_login = user
+    end
+
+  end
+end
+
+################################################################################
+ActionController::Base.send(:include, Mocking::ControllerInstanceMethods)
+ActionView::Base.send(:include, Mocking::ControllerInstanceMethods)
